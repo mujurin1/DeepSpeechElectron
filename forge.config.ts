4d576beb14dbe957417129a9ca85828e8abb99fd
mujurin1 <mujurin25@yahoo.co.jp>
2024-04-26 19:47:15 +0900

    公開用のセットアップ作成

--- forge.config.ts
+++ forge.config.ts
@@ -12,6 +12,19 @@ import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
   packagerConfig: {
     asar: true,
   },
